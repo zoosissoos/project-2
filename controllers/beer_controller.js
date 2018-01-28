cb1693949fc285d87ac41325537d2275ee74ea9f
dbrudner <dbrudne1@gmail.com>
2018-01-28 15:38:15 -0500

    singlebeer view

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -4,15 +4,23 @@ const router = express.Router();
 
 
 router.get('/', function(req, res) {
+    console.log("hi")
     res.render('index');
 })
 
 router.get('/add', function(req, res) {
+    console.log("Hi")
     res.render('add');
 })
 
 router.get('/search', function(req, res) {
+    console.log("Hi")    
     res.render('search');
+})
+
+router.get('/beer', function(req, res) {
+    console.log("Hi")    
+    res.render('singlebeer');
 })
 
 module.exports = router;
