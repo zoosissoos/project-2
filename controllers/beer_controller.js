db5df6afa165b59ff037415736919cdbefb41849
dbrudner <dbrudne1@gmail.com>
2018-01-28 20:45:51 -0500

    commit

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -4,23 +4,26 @@ const router = express.Router();
 
 
 router.get('/', function(req, res) {
-    console.log("hi")
     res.render('index');
 })
 
 router.get('/add', function(req, res) {
-    console.log("Hi")
+    console.log("Hi")    
     res.render('add');
 })
 
 router.get('/search', function(req, res) {
-    console.log("Hi")    
     res.render('search');
 })
 
 router.get('/beer', function(req, res) {
-    console.log("Hi")    
+    console.log("Hi")
     res.render('singlebeer');
+})
+
+router.get('/user', function(req, res) {
+    console.log("Hi")    
+    res.render('userprofile');
 })
 
 module.exports = router;
