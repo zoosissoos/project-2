4a466714693eb419cd4cedb1797d3480370585c9
dan <danbrianlewis@gmail.com>
2018-02-05 16:15:38 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "beers"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "beers"
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
